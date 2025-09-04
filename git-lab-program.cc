acbc8acde95ebec0f1068262eacaa70039b3cedb
Param Khanal <pk983524@ohio.edu>
2025-09-04 17:19:58 -0400

    Hello hello

---
+++ git-lab-program.cc
@@ -0,0 +1,19 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Param Khanal
+ *        Date: 4th September 2025
+ * Description: Add Description
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!!" << endl;
+    return 0;
+}// main
+
